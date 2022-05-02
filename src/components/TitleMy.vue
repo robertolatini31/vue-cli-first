b704da5b6497804f3458a69cf00258fc57f9d128
robertolatini31 <robertolatini31@gmail.com>
2022-05-02 14:36:05 +0200

    added my compontent of title

---
+++ src/components/TitleMy.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>La mia prima app con Vue CLI!</h1>
+        <img src="https://media.slid.es/thumbnails/b7ef6b29657f6990ef4b0747ccb07fff/thumb.jpg?1535032229" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+   name: 'TitleMy'
+}
+</script>
+
+<style scoped>
+</style>
